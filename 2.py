163b5ebfd89c0084d036c60460cf6c2e0dd3abf3
AlexsandrFK <sasha.furik.241@gmail.com>
2022-06-11 23:02:24 +1200

    второе  занятие

---
+++ 2.py
@@ -0,0 +1,15 @@
+# n = int(input('Please input integer: ')) 
+# b = ''
+# while n > 0:
+#     b = str(n % 2) + b
+#     n = n // 2
+# print(f'Binary number: {b}')
+
+# 3
+# import random
+ 
+# y = ['Шла ', 'Бежала ', 'Ехала ', 'Гуляла ']
+# random.shuffle(y)
+ 
+# print(y)
+
